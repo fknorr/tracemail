810be918733c67c4040d43fc5891c4f78dd43723
Fabian Knorr <fbknorr@gmail.com>
2017-09-03 20:38:35 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,30 +3,30 @@
 import re, os
 from setuptools import setup
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
-    with open("tracemail/__init__.py", "r") as file:
-        version = re.search('^__version__\s*=\s*"(.*)"', file.read(), re.M).group(1)
+    with open('tracemail/__init__.py', 'r') as file:
+        version = re.search(r'^__version__\s*=\s*\'(.*)\'', file.read(), re.M).group(1)
 
-    with open("README", "rb") as f:
-        long_descr = f.read().decode("utf-8")
+    with open('README', 'rb') as f:
+        long_descr = f.read().decode('utf-8')
 
     setup(
-        name = "tracemail",
-        packages = ["tracemail"],
+        name = 'tracemail',
+        packages = ['tracemail'],
         entry_points = {
-            "console_scripts": [ "studip = tracemail.cli:main" ]
+            'console_scripts': [ 'studip = tracemail.cli:main' ]
         },
         include_package_data = True,
         install_requires = [
-            "psycopg2",
-            "toml",
-            "flask"
+            'psycopg2',
+            'toml',
+            'flask'
         ],
         version = version,
-        description = "Disposable e-mail address management",
+        description = 'Disposable e-mail address management',
         long_description = long_descr,
-        author = "Fabian Knorr",
-        url = "https://github.com/fknorr/tracemail"
+        author = 'Fabian Knorr',
+        url = 'https://github.com/fknorr/tracemail'
     )
